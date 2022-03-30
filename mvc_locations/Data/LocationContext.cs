ae9bf7e23291e08e4e309d38e63038e3d7bb2785
Unicorn-isbad <69872616+Unicorn-isbad@users.noreply.github.com>
2022-03-30 18:29:05 +0200

    Add project files.

---
+++ mvc_locations/Data/LocationContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc_locations.Models;
+
+namespace mvc_locations.Data
+{
+    public class LocationContext : DbContext
+    {
+
+        public LocationContext(DbContextOptions<LocationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        // Zadatak:
+        // kreirati metodu OnModelCreating(ModelBuilder modelBuilder)
+        // Napraviti seed podataka za:
+        // Minimalno dvije nasumične države s pripadajućim podacima
+        // minimalno šest nasumičnih gradova s pripadajućim podacima
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+            
+        }
+    }
+}
