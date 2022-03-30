57b96fa7164687d215a9283956f25a65387d830f
Unicorn-isbad <69872616+Unicorn-isbad@users.noreply.github.com>
2022-03-30 18:48:24 +0200

    Done

--- mvc_locations/Data/LocationContext.cs
+++ mvc_locations/Data/LocationContext.cs
@@ -21,8 +21,17 @@ namespace mvc_locations.Data
         // minimalno šest nasumičnih gradova s pripadajućim podacima
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        { 
-            
+        {
+            modelBuilder.Entity<Country>().HasData(new Country { id = 1, country_name = "Kina", country_name_eng = "China", country_code = "CHN" },
+                                                   new Country { id = 2, country_name = "Hrvatska", country_name_eng = "Croatia", country_code = "HRV" },
+                                                   new Country { id = 3, country_name = "Uganda", country_name_eng = "Uganda", country_code = "UGA" });
+            modelBuilder.Entity<City>().HasData(new City { id = 1, country_id = 2, city_name = "Zagreb", latitude = 45.8150m, longitude = 15.9819m },
+                                                new City { id = 2, country_id = 2, city_name = "Split", latitude = 43.5147m, longitude = 16.4435m },
+                                                new City { id = 3, country_id = 1, city_name = "Peking", latitude = 39.9042m, longitude = 116.4074m },
+                                                new City { id = 4, country_id = 1, city_name = "Šangaj", latitude = 31.2304m, longitude = 121.4737m },
+                                                new City { id = 5, country_id = 3, city_name = "Kampala", latitude = 0.3476m, longitude = 32.5825m },
+                                                new City { id = 6, country_id = 3, city_name = "Gulu", latitude = 2.7724m, longitude = 32.2881m}
+                                                );
         }
     }
 }
---
+++ mvc_locations/Migrations/20220330164552_seed.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvc_locations.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "id", "country_code", "country_name", "country_name_eng" },
+                values: new object[] { 1, "CHN", "Kina", "China" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "id", "country_code", "country_name", "country_name_eng" },
+                values: new object[] { 2, "HRV", "Hrvatska", "Croatia" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "id", "country_code", "country_name", "country_name_eng" },
+                values: new object[] { 3, "UGA", "Uganda", "Uganda" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "id", "city_name", "country_id", "latitude", "longitude" },
+                values: new object[,]
+                {
+                    { 1, "Zagreb", 2, 45.8150m, 15.9819m },
+                    { 2, "Split", 2, 43.5147m, 16.4435m },
+                    { 3, "Peking", 1, 39.9042m, 116.4074m },
+                    { 4, "Šangaj", 1, 31.2304m, 121.4737m },
+                    { 5, "Kampala", 3, 0.3476m, 32.5825m },
+                    { 6, "Gulu", 3, 2.7724m, 32.2881m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "id",
+                keyValue: 3);
+        }
+    }
+}
